7d07752e4c5f6f965be1c1df41221e1df78f6c9d
Mateusz Wasilewski <01158977@pw.edu.pl>
2023-02-28 21:05:18 +0100

    Refactored files organizing from functions to Md5FilesOrganizer class

--- clean_directory.py
+++ clean_directory.py
@@ -2,7 +2,48 @@ import os
 import sys
 import hashlib
 
-FILES_ENCODING_FORMAT = 'UTF-8'
+
+class Md5FilesOrganizer:
+    def __init__(
+        self, root_dir: str = ".", files_encoding_format: str = "UTF-8"
+    ) -> None:
+        self.root = root_dir
+        self.files_encoding_format = files_encoding_format
+
+    def organize_files(self, root_dir: str = None) -> dict:
+        root_dir = root_dir if root_dir else self.root
+        files_by_md5 = dict()
+        self._handle_files(root_dir, files_by_md5)
+        return files_by_md5
+
+    def _handle_files(self, root_dir, files_by_md5):
+        for f in os.listdir(root_dir):
+            file_path = os.path.join(root_dir, f)
+            if os.path.isfile(file_path):
+                self._handle_single_file(files_by_md5, file_path)
+            if os.path.isdir(file_path):
+                self._handle_single_dir(files_by_md5, file_path)
+
+    def _handle_single_file(self, files_by_md5, file_path):
+        with open(file_path, "r") as file_handle:
+            encoded_file = file_handle.read().encode(
+                self.files_encoding_format
+            )
+            md5_hash = hashlib.md5(encoded_file).hexdigest()
+            self._update_files_by_md5_dict(files_by_md5, file_path, md5_hash)
+
+    def _handle_single_dir(self, files_by_md5, file_path):
+        subdir_files_by_md5 = self.organize_files(file_path)
+        for md5_hash in subdir_files_by_md5:
+            for md5_hash_file_path in subdir_files_by_md5[md5_hash]:
+                self._update_files_by_md5_dict(
+                    files_by_md5, md5_hash_file_path, md5_hash
+                )
+
+    def _update_files_by_md5_dict(self, files_by_md5, file_path, md5_hash):
+        if md5_hash not in files_by_md5.keys():
+            files_by_md5[md5_hash] = []
+        files_by_md5[md5_hash].append(file_path)
 
 
 def run(args) -> int:
@@ -12,32 +53,8 @@ def run(args) -> int:
         print("Invalid number of provided arguments")
         return 1
 
-    files_by_md5 = get_files_by_md5(args[1])
+    files_by_md5 = Md5FilesOrganizer(args[1]).organize_files()
     print(f"files_by_md5: {files_by_md5}")
-
-
-def get_files_by_md5(root_dir: str) -> dict:
-    files_by_md5 = dict()
-    for f in os.listdir(root_dir):
-        file_path = os.path.join(root_dir, f)
-        if os.path.isfile(file_path):
-            with open(file_path, 'r') as file_handle:
-                encoded_file = file_handle.read().encode(FILES_ENCODING_FORMAT)
-                md5_hash = hashlib.md5(encoded_file).hexdigest()
-
-                _update_files_by_md5_dict(files_by_md5, file_path, md5_hash)
-        if os.path.isdir(file_path):
-            subdir_files_by_md5 = get_files_by_md5(file_path)
-            for md5_hash in subdir_files_by_md5:
-                for md5_hash_file_path in subdir_files_by_md5[md5_hash]:
-                    _update_files_by_md5_dict(files_by_md5, md5_hash_file_path, md5_hash)
-    return files_by_md5
-
-
-def _update_files_by_md5_dict(files_by_md5, file_path, md5_hash):
-    if md5_hash not in files_by_md5.keys():
-        files_by_md5[md5_hash] = []
-    files_by_md5[md5_hash].append(file_path)
 
 
 if __name__ == "__main__":
