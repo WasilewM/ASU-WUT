d667dc74973d8ae95aaab1b45ea3529ff1c13464
Mateusz Wasilewski <01158977@pw.edu.pl>
2023-03-01 21:47:27 +0100

    Implemented FilesDataCollector class

--- clean_directory.py
+++ clean_directory.py
@@ -3,6 +3,74 @@ import sys
 import hashlib
 
 
+class FileData:
+    def __init__(
+        self, file_path: str, md5_hash: str, permissions: str
+    ) -> None:
+        self.file_path = file_path
+        self.md5_hash = md5_hash
+        self.permissions = permissions
+
+    def __repr__(self):
+        description = f"file_path: {self.file_path}, "
+        description += f"md5_hash: {self.md5_hash}, "
+        description += f"permissions: {self.permissions}"
+        return description
+
+
+class FilesDataCollector:
+    def __init__(
+        self, root_dir: str = ".", files_encoding_format: str = "UTF-8"
+    ) -> None:
+        self.root = root_dir
+        self.files_encoding_format = files_encoding_format
+
+    def get_files_data(self) -> list:
+        return self._find_files(self.root)
+
+    def _find_files(self, root_dir: str) -> list:
+        files = []
+        for f in os.listdir(root_dir):
+            file_path = os.path.join(root_dir, f)
+            if os.path.isfile(file_path):
+                self._handle_file(files, file_path)
+            elif os.path.isdir(file_path):
+                self._handle_dir(files, file_path)
+            else:
+                self._handle_unknown_obj(file_path)
+        return files
+
+    def _handle_file(self, files: list, file_path: str) -> None:
+        md5_hash = self._get_file_md5_hash(file_path)
+        permissions = self._get_file_permissions(file_path)
+        files.append(FileData(file_path, md5_hash, permissions))
+
+    def _get_file_md5_hash(self, file_path: str):
+        try:
+            with open(file_path, "r") as file_handle:
+                encoded_file = file_handle.read().encode(
+                    self.files_encoding_format
+                )
+                return hashlib.md5(encoded_file).hexdigest()
+        except Exception as e:
+            print(f"An error occurred while processing file {file_path}: {e}")
+
+    def _get_file_permissions(self, file_path: str) -> int:
+        try:
+            return os.stat(file_path).st_mode
+        except Exception as e:
+            print(f"An error occurred while processing file {file_path}: {e}")
+
+    def _handle_dir(self, files: list, file_path: str):
+        subdir_files = self._find_files(file_path)
+        subdir_files = subdir_files if subdir_files else []
+        files += subdir_files
+
+    def _handle_unknown_obj(self, file_path: str):
+        print(f"Object {file_path} could not be recognised.")
+        exit(1)
+
+
 class Md5FilesOrganizer:
     def __init__(
         self, root_dir: str = ".", files_encoding_format: str = "UTF-8"
@@ -58,41 +126,41 @@ class EmptyFilesRemover:
     def __init__(self, root_dir: str = ".") -> None:
         self.root = root_dir
 
-    def remove_empty_files(self):
-        empty_files = self._find_empty_files(self.root)
-        self._suggest_empty_files_removal(empty_files)
+    def remove_files(self):
+        files = self._find_files(self.root)
+        self._suggest_files_removal(files)
         user_answer = self._has_user_accepted_file_removal()
         if user_answer:
-            self._delete_empty_files(empty_files)
+            self._delete_files(files)
 
-    def _find_empty_files(self, root_dir: str) -> list:
-        empty_files = []
+    def _find_files(self, root_dir: str) -> list:
+        files = []
         for f in os.listdir(root_dir):
             file_path = os.path.join(root_dir, f)
             if os.path.isfile(file_path):
-                self._handle_file(empty_files, file_path)
+                self._handle_file(files, file_path)
             elif os.path.isdir(file_path):
-                self._handle_dir(empty_files, file_path)
+                self._handle_dir(files, file_path)
             else:
                 self._handle_unknown_obj(file_path)
-        return empty_files
+        return files
 
-    def _handle_file(self, empty_files: list, file_path: str):
+    def _handle_file(self, files: list, file_path: str):
         if os.path.getsize(file_path) == 0:
-            empty_files.append(file_path)
+            files.append(file_path)
 
-    def _handle_dir(self, empty_files: list, file_path: str):
-        subdir_empty_files = self._find_empty_files(file_path)
-        subdir_empty_files = subdir_empty_files if subdir_empty_files else []
-        empty_files += subdir_empty_files
+    def _handle_dir(self, files: list, file_path: str):
+        subdir_files = self._find_files(file_path)
+        subdir_files = subdir_files if subdir_files else []
+        files += subdir_files
 
     def _handle_unknown_obj(self, file_path: str):
         print(f"Object {file_path} could not be recognised.")
         exit(1)
 
-    def _suggest_empty_files_removal(self, empty_files: list) -> None:
+    def _suggest_files_removal(self, files: list) -> None:
         print("Do you want to delete following empty files?")
-        for file_path in empty_files:
+        for file_path in files:
             print(file_path)
         print("Y/N?")
 
@@ -106,8 +174,8 @@ class EmptyFilesRemover:
             print("Invalid option. Aborting the script...")
             exit(1)
 
-    def _delete_empty_files(self, empty_files: list) -> None:
-        for file_path in empty_files:
+    def _delete_files(self, files: list) -> None:
+        for file_path in files:
             os.remove(file_path)
             print(f"{file_path} has been removed")
 
@@ -130,9 +198,11 @@ def run(args: list) -> int:
         print("Invalid number of provided arguments")
         return 1
 
-    Md5FilesOrganizer(args[1]).organize_files()
-    EmptyFilesRemover(args[1]).remove_empty_files()
-    FilesPermissionsUpdater(args[1]).update_files_permissions()
+    files = FilesDataCollector(args[1]).get_files_data()
+    print(files)
+    # Md5FilesOrganizer(args[1]).organize_files()
+    # EmptyFilesRemover(args[1]).remove_files()
+    # FilesPermissionsUpdater(args[1]).update_files_permissions()
 
 
 if __name__ == "__main__":
