7127088bcf03c41b5cc684fe8fdd28b1059098d2
Mateusz Wasilewski <01158977@pw.edu.pl>
2023-03-12 22:01:23 +0100

    Simplified FilesRenamer class

--- actions/files_renamer.py
+++ actions/files_renamer.py
@@ -12,10 +12,7 @@ class FilesRenamer:
     def replace_unwanted_chars(self) -> None:
         for file in self.files:
             dirs, file_name = os.path.split(file.file_path)
-            (
-                new_file_name,
-                file_extension,
-            ) = self._separate_file_name_from_extension(file_name)
+            new_file_name, file_extension = os.path.splitext(file_name)
             for c in self.unwanted_chars:
                 if c in new_file_name:
                     new_file_name = new_file_name.replace(
@@ -25,10 +22,3 @@ class FilesRenamer:
             new_file_path = os.path.join(dirs, new_file_name)
             os.rename(file.file_path, new_file_path)
             print(f"File {file.file_path} has been renamed to {new_file_path}")
-
-    def _separate_file_name_from_extension(self, file_name: str) -> tuple:
-        idx = len(file_name) - 1
-        while idx >= 0 and file_name[idx] != ".":
-            idx -= 1
-
-        return file_name[:idx], file_name[idx:]
