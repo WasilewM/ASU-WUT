e56e9e18d0a7e91163b19b25b2edd3511b3ca9ef
Mateusz Wasilewski <01158977@pw.edu.pl>
2023-03-12 22:02:05 +0100

    Implemented FilesRelocator

--- actions/files_relocator.py
+++ actions/files_relocator.py
@@ -1,7 +1,18 @@
+from .files_data_collector import FileData
+import os
+import shutil
+
+
 class FilesRelocator:
     def __init__(self, files: list, new_dir: str) -> None:
         self.files = files
         self.new_dir = new_dir
 
     def relocate_files(self) -> None:
-        print("Implement me!")
+        for file in self.files:
+            self._relocate_file(file)
+
+    def _relocate_file(self, file: FileData) -> None:
+        file_name = os.path.split(file.file_path)[-1]
+        new_file_path = os.path.join(self.new_dir, file_name)
+        shutil.move(file.file_path, new_file_path)
