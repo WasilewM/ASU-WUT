834451eea396e5e8cf494d2cc3ef717ad8e91f57
Mateusz Wasilewski <01158977@pw.edu.pl>
2023-03-29 21:39:01 +0200

    Refactored FilesPermissionsUpdater

--- actions/files_permissions_updater.py
+++ actions/files_permissions_updater.py
@@ -1,4 +1,4 @@
-from .action_base_io import ActionBaseIO
+from .action_base_io import ActionBaseIO, UserActionAnswerEnum
 from .files_data_collector import FileData
 import os
 
@@ -7,24 +7,34 @@ class FilesPermissionsUpdater(ActionBaseIO):
     def __init__(self, files: list, expected_permissions: int = 644) -> None:
         self.files = files
         self.expected_permissions = expected_permissions
+        self.apply_action_to_all = False
 
     def update_files_permissions(self) -> None:
+        for file in self.files:
+            if not self.apply_action_to_all:
+                self._handle_individual_file_permissions_update(file)
+            else:
+                self._update_file_permissions(file)
+
+    def _handle_individual_file_permissions_update(self, file: FileData):
         self._sugest_action_on_files(
             "Do you want to update permissions for following files?",
-            self.files,
+            file.file_path,
         )
         user_answer = self._has_user_accepted_the_action()
-        if user_answer:
-            for file in self.files:
-                if file.permissions != self.expected_permissions:
-                    self._update_file_permissions(file)
+        if user_answer == UserActionAnswerEnum.YES:
+            self._update_file_permissions(file)
+        elif user_answer == UserActionAnswerEnum.APPLY_TO_ALL:
+            self.apply_action_to_all = True
+            self._update_file_permissions(file)
 
     def _update_file_permissions(self, file: FileData) -> None:
-        os.chmod(file.file_path, self._get_octal_permissions())
-        print(
-            f"File {file.file_path} has been updated to have following "
-            + f"permissions: {self.expected_permissions}"
-        )
+        if file.permissions != self.expected_permissions:
+            os.chmod(file.file_path, self._get_octal_permissions())
+            print(
+                f"File {file.file_path} has been updated to have following "
+                + f"permissions: {self.expected_permissions}"
+            )
 
     def _get_octal_permissions(self) -> int:
         return int(str(self.expected_permissions), base=8)
--- actions/files_remover.py
+++ actions/files_remover.py
@@ -6,24 +6,24 @@ import os
 class FilesRemover(ActionBaseIO):
     def __init__(self, files: list) -> None:
         self.files = files
-        self.delete_all = False
+        self.apply_action_to_all = False
 
     def remove_files(self) -> None:
         for file in self.files:
-            if not self.delete_all:
+            if not self.apply_action_to_all:
                 self._handle_individual_files_removal(file)
             else:
                 self._remove_file(file)
 
     def _handle_individual_files_removal(self, file: FileData):
         self._sugest_action_on_files(
-                    "Do you want to delete following file?", file.file_path
-                )
+            "Do you want to delete following file?", file.file_path
+        )
         user_answer = self._has_user_accepted_the_action()
         if user_answer == UserActionAnswerEnum.YES:
             self._remove_file(file)
         elif user_answer == UserActionAnswerEnum.APPLY_TO_ALL:
-            self.delete_all = True
+            self.apply_action_to_all = True
             self._remove_file(file)
 
     def _remove_file(self, file: FileData):
